fd0ccd4265707001dec401fcd02ef94f72758b8c
Elia Toribio Guillén <20198852@itla.edu.do>
2023-11-25 02:21:56 -0400

    Rutas y views agregadas

--- routes/dev.js
+++ routes/dev.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const DevController = require('../controllers/DevController')
 
 router.get("/developers", DevController.GetDeveloperList);
-
+// router.get("/create-developer", DeveloperController.GetCreateDeveloper);
+// router.post("/create-developer", DeveloperController.PostCreateDeveloper);
+// router.get("/edit-developer/:developerId", DeveloperController.GetEditDeveloper);
+// router.post("/edit-developer", DeveloperController.PostEditDeveloper);
+// router.post("/delete-developer", DeveloperController.PostDeleteDeveloper);
 
 module.exports = router;
--- routes/game.js
+++ routes/game.js
@@ -5,6 +5,11 @@ const GameController = require('../controllers/GameController')
 
 router.get("/", GameController.GetIndex);
 router.get("/games", GameController.GetGamesList);
+// router.get("/create-game", GameController.GetCreateGame);
+// router.post("/create-game", GameController.PostCreateGame);
+// router.get("/edit-game/:gameId", GameController.GetEditGame);
+// router.post("/edit-game", GameController.PostEditGame);
+// router.post("/delete-game", GameController.PostDeleteGame);
 
 
 module.exports = router;
